247df4d27db7b8488ff96bb92ec3a4d0b4bb7422
Shady Medhat <shadymedhat971128@gmail.com>
2026-07-03 09:53:11 +0300

    init the mobile app

---
+++ mobile-app/metro.config.js
@@ -0,0 +1,4 @@
+const {getDefaultConfig} = require('expo/metro-config');
+const {withNativeWind} = require('nativewind/metro');
+const defaultConfig = getDefaultConfig(__dirname);
+module.exports = withNativeWind(defaultConfig, {input:"./global.css"});
